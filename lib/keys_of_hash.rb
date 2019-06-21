05dae59d097d0b5350561330bfb2cab3b044224a
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-21 00:11:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    result = []
+    if Hash.has_value?(arguments)
+      result.push(Hash.index(arguments))
+    end
   end
 end
